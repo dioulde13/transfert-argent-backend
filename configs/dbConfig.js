c7edaa349c64abc2a89d865bdfc2981e6aba222e
ecash <dioulde.diallo@ecash-guinee.com>
2025-08-16 10:42:32 +0000

    config

--- configs/dbConfig.js
+++ configs/dbConfig.js
@@ -1,19 +1,19 @@
-// require("dotenv").config();
+require("dotenv").config();
 
-// module.exports = {
-//   host: process.env.MYSQLHOST,
-//   user: process.env.MYSQLUSER,         // 👈 attention ici
-//   password: process.env.MYSQLPASSWORD,
-//   database: process.env.MYSQLDATABASE,
-//   port: parseInt(process.env.MYSQLPORT || "3306"),
-// };
+module.exports = {
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,         // 👈 attention ici
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: parseInt(process.env.MYSQLPORT || "3306"),
+};
 
 // Configuration de la connexion à la base de données
-const dbConfig = {
-  port: 3306,
-  host: '127.0.0.1',
-  user: 'root',
-  password: '1234',
-  database: 'transferts',
-};
-module.exports = dbConfig;
+// const dbConfig = {
+//   port: 3306,
+//   host: '127.0.0.1',
+//   user: 'root',
+//   password: '1234',
+//   database: 'transferts',
+// };
+// module.exports = dbConfig;
